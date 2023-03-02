1284097b94e2c7d32fcca6b263c4f90c71a10a50
Ubaydul Haque Sami <64384070+samihaque@users.noreply.github.com>
2023-03-01 19:42:58 -0500

    end of 3/1/23 class.Learned adding JFX & component on react. one issue needs to be resolved ({sayHello()} works but <sayHello /> doesn't work inside app function.

--- myfirst-react-app/myfirst-react-app/src/App.js
+++ myfirst-react-app/myfirst-react-app/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import sayHello from './component/hello'
 
 function App() {
+  let name = "sam"
+  // return JSX
   return (
     <div className="App">
       <header className="App-header">
       <h1>Adding a Header to my app!😮‍💨</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          yoo!! {name} is excided to hack in react.
         </p>
+        {sayHello()}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,8 +23,11 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
+
+
 
 export default App;
---
+++ myfirst-react-app/myfirst-react-app/src/component/hello.js
@@ -0,0 +1,14 @@
+let sayHello=()=>{
+    let message ="hello"
+    console.log(message)
+    return (
+      <div >
+        <h3 >{message}</h3>
+        <p>This is sami here.....</p>
+        <p>Writing from another file</p>
+      </div>
+  
+    )
+  }
+
+  export default sayHello
